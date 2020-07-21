ce022a795870d54bf61d48c0f5292caefeec0bf9
Alexis3004 <nelsonalexiscacerzcarreo37@gmail.com>
2020-07-21 12:02:58 -0500

    logo en titulo y cambio de nombre html

---
+++ default.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, user-scalable=no"
+    />
+    <link rel="shortcut icon" type="image/png" href="img/llama.png">
+    <title>𝖋𝖑𝖆𝖒𝖒𝖆</title>
+    <meta name="description" content="SI NO QUEMA, NO ES ARTE."><!-- /snippets/social-meta-tags.liquid -->
+    <meta name="keywords" content="flamma,flammashop, www.flammashop.com, flammashop.com, "/>
+    <link rel="styleSheet" href="css/estilos.css" _ />
+  </head>
+  <body>
+    <div class="contenedor">
+      <div id="logo">flamma</div>
+      <div id="proximamente">Próximamente</div>
+      <div id="redes-sociales">
+        <div id="facebook">
+          <a href="https://www.facebook.com/flamma0" target="_blank"
+            ><img src="img/fbW.png" alt="Facebook"
+          /></a>
+        </div>
+        <div id="instagram">
+          <a href="https://www.instagram.com/___flamma___/" target="_blank"
+            ><img src="img/igW.png" alt="Instagram"
+          /></a>
+        </div>
+        <div id="whatsapp">
+          <a
+            href="https://api.whatsapp.com/send?phone=573187251299"
+            target="_blank"
+            ><img src="img/wsW.png" alt="whatsapp"
+          /></a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
